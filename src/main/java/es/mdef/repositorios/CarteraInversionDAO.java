d4f7bb3fc998f132515c14146991576a0ce7e5b9
agarca4 <carrillofuente@hotmail.com>
2020-04-16 23:20:25 +0200

    algunos cambios menores

--- src/main/java/es/mdef/repositorios/CarteraInversionDAO.java
+++ src/main/java/es/mdef/repositorios/CarteraInversionDAO.java
@@ -1,9 +1,10 @@
 package es.mdef.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import es.mdef.CarteraInversion;
 
+@Repository
 public interface CarteraInversionDAO extends JpaRepository<CarteraInversion, String> {
 
 }
--- src/main/java/es/mdef/repositorios/ImportadorDAO.java
+++ src/main/java/es/mdef/repositorios/ImportadorDAO.java
@@ -2,7 +2,6 @@ package es.mdef.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import es.mdef.Importador;
 
 @Repository
