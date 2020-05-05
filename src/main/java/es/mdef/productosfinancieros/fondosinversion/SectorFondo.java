bcaa10b6d75b0039970edc32d103ede5a6579a1d
agarca4 <carrillofuente@hotmail.com>
2020-05-05 19:44:16 +0200

    borrando

--- src/main/java/es/mdef/productosfinancieros/fondosinversion/SectorFondo.java
+++
@@ -1,6 +0,0 @@
-package es.mdef.productosfinancieros.fondosinversion;
-
-public enum SectorFondo {
-	TECNOLOGIA, CONSUMO_DEFENSIVO, INDUSTRIA, CONSUMO_CICLICO, SALUD, SERVICIOS_FINANCIEROS
-
-}
--- src/main/java/es/mdef/productosfinancieros/fondosinversion/TipoFondo.java
+++
@@ -1,7 +0,0 @@
-package es.mdef.productosfinancieros.fondosinversion;
-
-public enum TipoFondo {
-	
-	RENTA_FIJA, RENTA_VARIABLE, MIXTO
-
-}
