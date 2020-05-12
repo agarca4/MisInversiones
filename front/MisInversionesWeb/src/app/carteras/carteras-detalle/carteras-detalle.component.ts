a2a91b265b79187b81f63c11fe740e745eadc188
agarca4 <carrillofuente@hotmail.com>
2020-05-12 11:37:36 +0200

    entrega back-end

--- front/MisInversionesWeb/src/app/carteras/carteras-detalle/carteras-detalle.component.ts
+++ front/MisInversionesWeb/src/app/carteras/carteras-detalle/carteras-detalle.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cartera } from 'src/app/modelo/cartera';
+import { CarterasService } from 'src/app/servicios/carteras.service';
 
 @Component({
   selector: 'app-carteras-detalle',
@@ -8,12 +9,20 @@ import { Cartera } from 'src/app/modelo/cartera';
 })
 export class CarterasDetalleComponent implements OnInit {
 
-  idCartera = "1";
-  cartera: Cartera;
+  idCartera = "23";
+  cartera: Cartera = {
+    nombre: null,
+    fechaCreacionCartera: null,
+    capitalInvertido: null,
+    rentabilidad: null
+  };
 
-  constructor() { }
+  constructor(private carterasService: CarterasService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.carterasService.getCarteraPorId(this.idCartera).subscribe(
+      respuesta => this.cartera = respuesta
+    )
   }
 
 }
