b07f5c1b10c63fce26f2b8c512030518751ae7e7
agarca4 <carrillofuente@hotmail.com>
2020-05-21 15:40:12 +0200

    cross

--- src/main/java/es/mdef/repositorios/UsuarioDAO.java
+++ src/main/java/es/mdef/repositorios/UsuarioDAO.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import es.mdef.usuarios.Usuario;
 
-@CrossOrigin(origins = { "http://localhost:4200/", "https://agarca4.github.io/" })
+@CrossOrigin(origins = { "http://localhost:4200", "https://agarca4.github.io" })
 @RepositoryRestResource(path = "usuarios", itemResourceRel = "usuario", collectionResourceRel = "usuarios")
 public interface UsuarioDAO extends JpaRepository<Usuario, String> {
 
