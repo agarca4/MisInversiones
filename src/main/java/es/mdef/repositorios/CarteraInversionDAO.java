cc32757945f3ed0945789beb89f26cf746c63cf3
agarca4 <carrillofuente@hotmail.com>
2020-05-21 14:34:12 +0200

    tocando el cross

--- src/main/java/es/mdef/repositorios/CarteraInversionDAO.java
+++ src/main/java/es/mdef/repositorios/CarteraInversionDAO.java
@@ -10,7 +10,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import es.mdef.CarteraInversion;
 
-@CrossOrigin(origins = { "http://localhost:4200", "https://agarca4.github.io" })
+@CrossOrigin(origins = {"http://localhost:4200" , "https://agarca4.github.io"})
 @RepositoryRestResource(path="carteras", itemResourceRel = "cartera", collectionResourceRel = "carteras")
 public interface CarteraInversionDAO extends JpaRepository<CarteraInversion, String> {
 	
