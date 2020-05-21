5925dcab836ba63d7ac50a43a486fef7214b8907
agarca4 <carrillofuente@hotmail.com>
2020-05-21 08:25:25 +0200

    desplegando eh heroku

--- src/main/java/es/MisInversionesApplication.java
+++ src/main/java/es/MisInversionesApplication.java
@@ -34,42 +34,40 @@ import es.mdef.usuarios.Usuario;
 @ImportResource({ "classpath:config/jpa-config.xml" })
 public class MisInversionesApplication {
 
-//	private static GestorCartera<FondoInversion, CarteraInversion> miGestorCartera = new GestorCarteraImpl(
-//			"agresiva");
-//
-//	private static GestorCartera<FondoInversion, CarteraInversion> getMiGestorCartera() {
-//		return miGestorCartera;
-//	}
+	private static GestorCartera<FondoInversion, CarteraInversion> miGestorCartera = new GestorCarteraImpl(
+			"agresiva");
+
+	private static GestorCartera<FondoInversion, CarteraInversion> getMiGestorCartera() {
+		return miGestorCartera;
+	}
 
 	public static void main(String[] args) throws ParseException, IOException {
 
 		ConfigurableApplicationContext context = SpringApplication.run(MisInversionesApplication.class, args);
 
-//		FondoInversion fondo1 = new FondoInversion("SP500", 150.00, SectorFondo.CONSUMO_CICLICO,
-//				TipoFondo.RENTA_VARIABLE);
-//		FondoInversion fondo2 = new FondoInversion("Edeficandi", 52.00, SectorFondo.INDUSTRIA, TipoFondo.RENTA_FIJA);
-//		FondoInversion fondo3 = new FondoInversion("MidTem", 18.00, SectorFondo.CONSUMO_DEFENSIVO, TipoFondo.MIXTO);
+		FondoInversion fondo1 = new FondoInversion("SP500", 150.00, SectorFondo.CONSUMO_CICLICO,
+				TipoFondo.RENTA_VARIABLE);
+		FondoInversion fondo2 = new FondoInversion("Edeficandi", 52.00, SectorFondo.INDUSTRIA, TipoFondo.RENTA_FIJA);
+		FondoInversion fondo3 = new FondoInversion("MidTem", 18.00, SectorFondo.CONSUMO_DEFENSIVO, TipoFondo.MIXTO);
 		
-//		Usuario usuario1 = new Usuario("Juan");
-//		Usuario usuario2 = new Usuario("Victoria");
-//		Usuario usuario3 = new Usuario("Belen");
-//		
-//		getMiGestorCartera().altaUsuario(usuario1);
-//		getMiGestorCartera().altaUsuario(usuario2);
-//		getMiGestorCartera().altaUsuario(usuario3);
-//		getMiGestorCartera().bajaUsuario(usuario1);
+		Usuario usuario1 = new Usuario("Juan");
+		Usuario usuario2 = new Usuario("Victoria");
+		Usuario usuario3 = new Usuario("Belen");
+		
+		getMiGestorCartera().altaUsuario(usuario1);
+		getMiGestorCartera().altaUsuario(usuario2);
+		getMiGestorCartera().altaUsuario(usuario3);
+		getMiGestorCartera().bajaUsuario(usuario1);
 
-//		getMiGestorCartera().compraProductoFinanciero(fondo1, 1500.00);
-//		getMiGestorCartera().compraProductoFinanciero(fondo2, 2000.00);
-//		getMiGestorCartera().compraProductoFinanciero(fondo3, 2000.00);
-//		getMiGestorCartera().vendeProductoFinanciero(fondo2, 1000.00);
-//
-//		
-//
-//		
-//		CarteraInversionDAO miCartera = context.getBean(CarteraInversionDAO.class);
-//		miCartera.deleteAll();
-//		miCartera.save(miGestorCartera.getCartera());
+		getMiGestorCartera().compraProductoFinanciero(fondo1, 1500.00);
+		getMiGestorCartera().compraProductoFinanciero(fondo2, 2000.00);
+		getMiGestorCartera().compraProductoFinanciero(fondo3, 2000.00);
+		getMiGestorCartera().vendeProductoFinanciero(fondo2, 1000.00);
+
+	
+		CarteraInversionDAO miCartera = context.getBean(CarteraInversionDAO.class);
+		miCartera.deleteAll();
+		miCartera.save(miGestorCartera.getCartera());
 
 		// context.close();
 
